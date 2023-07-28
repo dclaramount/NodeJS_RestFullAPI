1cbe93a7103cc6329274d12560e629d96345755b
Diego Claramount <dieg.ruiz@praguelabs.com>
2023-07-28 11:05:58 +0200

    mongoDB: add user

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require(`express`);
+const User = require(`./db/userModel`);
 
 const app = express();
 
 const dbConnect = require(`./db/dbConnect`);
 
 dbConnect()
+const newUser = new User({
+  email: `john@example.com`,
+  password: `string123`
+})
+
+newUser.save()
+  .then(() => {
+    console.log(`User inserted succesfully`)
+  })
+  .catch((error) => {
+    console.log(`There was an error inserting a field with error ${error}`)
+  })
 
 module.exports = app;
